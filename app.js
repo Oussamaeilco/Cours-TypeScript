cf43638be76b60ff68edefb6f0292a4fae952e8b
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-11 23:39:13 +0100

    First Commit
    First commit to try and make sure that all my work is up-to-date.

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log("Enter code her..");
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add( n1 : number, n2 : number){
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 4.5;
+
+const result =add(number1, number2);

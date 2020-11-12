a25014109e1189b64c1bbc6ec517e805bc010858
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-12 10:18:14 +0100

    Small changes

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
-function add( n1 : number, n2 : number){
-    return n1 + n2;
+function add(n1: number, n2: number, showResult?: boolean) {
+  return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 4.5;
+const printResult = true;
 
-const result =add(number1, number2);
+const result1 = add(number1, number2);
+
+const result2 = add(number1, number2, printResult);

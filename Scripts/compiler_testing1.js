0b603af586c113673be29253aae49c5d183303d4
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-12 12:42:00 +0100

    Modifiying the tsconfig to only compile the app.js file

--- Scripts/compiler_testing1.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// watch mode for TS compiler tsc app.js -w
-console.log('app.ts is compiled!');
--- Scripts/compiler_testing2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//To run TS compiler globly on the project tsc --init
-console.log("analytics.ts is compiled!");
-console.log("Yay tsc -w is working!");

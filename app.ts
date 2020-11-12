0b02b1a921486b3659d54bb0c68a0e4ac4f27af7
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-12 11:29:18 +0100

    Studying functions

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-
+/* 
 // function add(n1: number, n2: number, showResult?: boolean) {
 //   return n1 + n2;
 // }
@@ -24,35 +24,52 @@
 //     role: [2, 'author']
 // };
 
-enum Role{
-    Admin,
-    Author,
-    Read_Only
-}
-
-
-const person ={
-    name: 'Muffin_Time',
-    age: 26,
-    hobbies: ['Reading','Music'],
-    role : Role.Author
-}
-
-console.log(person.name);
-
-for(const hobby of person.hobbies){
-    console.log(hobby);
-}
-
-// if(person.role===Role.Admin){
-//     console.log('Is an Admin');
+// enum Role{
+//     Admin,
+//     Author,
+//     Read_Only
 // }
 
-switch(person.role){
-    case Role.Admin: console.log('Is an Admin!');
-                     break;
-    case Role.Author: console.log('Is an Author!');
-    break;
-    case Role.Read_Only: console.log('Is an Reader!');
-    break;
+
+// const person ={
+//     name: 'Muffin_Time',
+//     age: 26,
+//     hobbies: ['Reading','Music'],
+//     role : Role.Author
+// }
+
+// console.log(person.name);
+
+// for(const hobby of person.hobbies){
+//     console.log(hobby);
+// }
+
+// // if(person.role===Role.Admin){
+// //     console.log('Is an Admin');
+// // }
+
+// switch(person.role){
+//     case Role.Admin: console.log('Is an Admin!');
+//                      break;
+//     case Role.Author: console.log('Is an Author!');
+//     break;
+//     case Role.Read_Only: console.log('Is an Reader!');
+//     break;
+// } */
+
+//function with a return value
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
+
+//function without a return value (basicaly a methode)
+function printResult(num: number) : void{
+    console.log("Result = "+num);
+}
+
+//Testing functions
+printResult(add(3.14,380));
+
+//We can asign a function to variable 
+let combineValue : Function = add;
+console.log(combineValue(89,5));

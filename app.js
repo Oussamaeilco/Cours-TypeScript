f21beb4a9442e82bd30980eb58eb3df4edb5904d
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-12 10:28:50 +0100

    Smaller changes

---
+++ app.js
@@ -0,0 +1,8 @@
+function add(n1, n2, showResult) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 4.5;
+var printResult = true;
+var result1 = add(number1, number2);
+var result2 = add(number1, number2, printResult);

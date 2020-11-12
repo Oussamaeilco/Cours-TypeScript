f069031016550582868b8347d6743007385ff93c
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-12 12:42:00 +0100

    Modifiying the tsconfig to only compile the app.js file

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -2,5 +2,5 @@ const button =document.querySelector("button")!;
 
 
 button.addEventListener('click', ()=>{
-    console.log("You couldn't help yourself!")
+    console.log("You couldn't help yourself!");
 });

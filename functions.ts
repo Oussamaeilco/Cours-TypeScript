11d3a375d94c47e68f481e20bff7b6b948b7f49e
Oussamaeilco <51536532+Oussamaeilco@users.noreply.github.com>
2020-11-12 11:47:59 +0100

    Final Commit for functions

---
+++ functions.ts
@@ -0,0 +1,30 @@
+//function with a return value
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+//function without a return value (basicaly a methode)
+function printResult(num: number): void {
+  console.log("printResult = " + num);
+}
+
+//Testing functions
+printResult(add(3.14, 380));
+
+//We can asign a function to variable
+// let combineValue : Function = add;
+
+//Function type in typescript
+let combineValue: (x: number, y: number) => number;
+combineValue = add;
+console.log("CombineValue = "+combineValue(89, 5));
+
+//Callback and how it works
+function addAndHandle(x: number, y: number, cb:(res: number) => void) {
+  const result = x + y;
+  cb(result);
+}
+
+addAndHandle(10,5, (result)=>{
+    console.log("addAndHandle = " + result);
+});
